4de91523a8f2962532a3b0e99c5d9b07e4d0c8a5
Adam Zs <33822153+zsarnoczay@users.noreply.github.com>
2019-10-13 20:46:11 -0700

    azs - move DM and DV output writing from DL_calculation to pelicun

--- pelicun/file_io.py
+++ pelicun/file_io.py
@@ -52,6 +52,8 @@ This module has classes and methods that handle file input and output.
     create_HAZUS_EQ_json_files
     create_HAZUS_HU_json_files
     write_SimCenter_DL_output
+    write_SimCenter_DM_output
+    write_SimCenter_DV_output
 
 """
 
@@ -63,6 +65,16 @@ from distutils.util import strtobool
 from copy import deepcopy
 
 import warnings
+
+convert_dv_name = {
+    'DV_rec_cost': 'Reconstruction Cost',
+    'DV_rec_time': 'Reconstruction Time',
+    'DV_injuries_0': 'Injuries lvl. 1',
+    'DV_injuries_1': 'Injuries lvl. 2',
+    'DV_injuries_2': 'Injuries lvl. 3',
+    'DV_injuries_3': 'Injuries lvl. 4',
+    'DV_red_tag': 'Red Tag ',
+}
 
 # this is a convenience function for converting strings to float or None
 def float_or_None(string):
@@ -2095,3 +2107,99 @@ def write_SimCenter_DL_output(output_path, output_df, index_name='#Num',
     # write the results in a csv file
     # TODO: provide other file formats
     output_df.to_csv(output_path)
+
+def write_SimCenter_DM_output(DM_file_path, DMG_df):
+
+    # Start with the probability of being in a particular damage state.
+    # Here, the damage state of the building (asset) is defined as the highest
+    # damage state among the building components/component groups. This works
+    # well for a HAZUS assessment, but something more sophisticated is needed
+    # for a FEMA P58 assessment.
+
+    # Determine the probability of DS exceedance by collecting the DS from all
+    # components and assigning ones to all lower damage states.
+    DMG_agg = DMG_df.T.groupby('DS').sum().T
+    DMG_agg[DMG_agg > 0.0] = DMG_agg[DMG_agg > 0.0] / DMG_agg[DMG_agg > 0.0]
+
+    cols = DMG_agg.columns
+    for i in range(len(cols)):
+        filter = np.where(DMG_agg.iloc[:,i].values > 0.0)[0]
+        DMG_agg.iloc[filter,idx[0:i]] = 1.0
+
+    # The P(DS=ds) probability is determined by subtracting consecutive DS
+    # exceedance probabilites. This will not work well for a FEMA P58 assessment
+    # with Damage State Groups that include multiple Damage States.
+    DMG_agg_mean = DMG_agg.describe().loc['mean',:]
+    DS_0 = 1.0 - DMG_agg_mean['1-1']
+    for i in range(len(DMG_agg_mean.index)-1):
+        DMG_agg_mean.iloc[i] = DMG_agg_mean.iloc[i] - DMG_agg_mean.iloc[i+1]
+
+    # Add the probability of no damage for convenience.
+    DMG_agg_mean['0'] = DS_0
+    DMG_agg_mean = DMG_agg_mean.sort_index()
+
+    # Save the results in the output json file
+    DM = {'aggregate': {}}
+
+    for id in DMG_agg_mean.index:
+        DM['aggregate'].update({str(id): DMG_agg_mean[id]})
+
+    # Now determine the probability of being in a damage state for individual
+    # components / component assemblies...
+    DMG_mean = DMG_df.describe().loc['mean',:]
+
+    # and save the results in the output json file.
+    for FG in sorted(DMG_mean.index.get_level_values('FG').unique()):
+        DM.update({str(FG):{}})
+
+        for PG in sorted(
+            DMG_mean.loc[idx[FG],:].index.get_level_values('PG').unique()):
+            DM[str(FG)].update({str(PG):{}})
+
+            for DS in sorted(
+                DMG_mean.loc[idx[FG],:].loc[idx[:,PG],:].index.get_level_values('DS').unique()):
+                DM[str(FG)][str(PG)].update({str(DS): DMG_mean.loc[(FG,PG,DS)]})
+
+    with open(DM_file_path, 'w') as f:
+        json.dump(DM, f, indent = 2)
+
+def write_SimCenter_DV_output(DV_file_path, DV_df, DV_name):
+
+    DV_name = convert_dv_name[DV_name]
+
+    try:
+        with open(DV_file_path, 'r') as f:
+            DV = json.load(f)
+    except:
+        DV = {}
+
+    DV.update({DV_name: {}})
+
+    DV_i = DV[DV_name]
+
+    try:
+        DV_tot = DV_df.sum(axis=1).describe([0.1,0.5,0.9]).drop('count')
+        DV_i.update({'total':{}})
+        for stat in DV_tot.index:
+            DV_i['total'].update({stat: DV_tot.loc[stat]})
+
+        DV_stats = DV_df.describe([0.1,0.5,0.9]).drop('count')
+        for FG in sorted(DV_stats.columns.get_level_values('FG').unique()):
+            DV_i.update({str(FG):{}})
+
+            for PG in sorted(
+                DV_stats.loc[:,idx[FG]].columns.get_level_values('PG').unique()):
+                DV_i[str(FG)].update({str(PG):{}})
+
+                for DS in sorted(
+                    DV_stats.loc[:,idx[FG, PG]].columns.get_level_values('DS').unique()):
+                    DV_i[str(FG)][str(PG)].update({str(DS): {}})
+                    DV_stats_i = DV_stats.loc[:,(FG,PG,DS)]
+                    for stat in DV_stats_i.index:
+                        DV_i[str(FG)][str(PG)][str(DS)].update({
+                            stat: DV_stats_i.loc[stat]})
+    except:
+        pass
+
+    with open(DV_file_path, 'w') as f:
+        json.dump(DV, f, indent = 2)
