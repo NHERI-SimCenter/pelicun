e98b3772747a5f575b4d84fd9b9499bc5cd69ea4
Adam Zs <33822153+zsarnoczay@users.noreply.github.com>
2019-10-14 20:29:53 -0700

    azs - update the version tag to 2.0

--- pelicun/__init__.py
+++ pelicun/__init__.py
@@ -39,7 +39,7 @@
 
 name = "pelicun"
 
-__version__ = '1.1.7'
+__version__ = '2.0'
 
 __copyright__ = """Copyright (c) 2018 Leland Stanford Junior University and 
 The Regents of the University of California"""
