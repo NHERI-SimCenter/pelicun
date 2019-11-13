8a9b9caa7141127cd0e65add0bd30e57b5dd76d4
Adam Zs <33822153+zsarnoczay@users.noreply.github.com>
2019-11-13 14:59:50 -0800

    azs - extend auto population for HAZUS HU

--- pelicun/auto.py
+++ pelicun/auto.py
@@ -77,7 +77,7 @@ ap_RoofType = {
     'hipped': 'hip',
     'gabled': 'gab',
     'gable': 'gab',
-    'flat': 'hip'   # we need to change this later!
+    'flat': 'flt'   # we need to change this later!
 }
 
 convert_design_level = {
@@ -87,10 +87,14 @@ convert_design_level = {
         'Pre-Code'     : 'PC'
     }
 
-def auto_populate(DL_input_path, DL_method, realization_count):
+def auto_populate(DL_input_path, EDP_input_path,
+                  DL_method, realization_count):
 
     with open(DL_input_path, 'r') as f:
         DL_input = json.load(f)
+
+    EDP_input = pd.read_csv(EDP_input_path, sep='\s+', header=0,
+                            index_col=0)
 
     if 'GeneralInformation' in DL_input.keys():
         BIM_in = DL_input['GeneralInformation']
@@ -176,6 +180,8 @@ def auto_populate(DL_input_path, DL_method, realization_count):
         bldg_desc = BIM_in['buildingDescription']
         struct_type = BIM_in['structureType']
         V_ult = BIM_in['V_design']
+        area = BIM_in['area']
+        z0 = BIM_in['z0']
 
         # Meta-variables
         V_asd = 0.6 * V_ult
@@ -191,78 +197,214 @@ def auto_populate(DL_input_path, DL_method, realization_count):
                ((V_ult > 140.0) or ((V_ult > 130.0) and (FR))))
 
         # attributes for WSF 1-2
+        if ((roof_type != 'flt') and
+            (stories <= 2) and
+            (area < 2000.0)):
+            # Secondary water resistance
+            SWR = np.random.binomial(1, 0.6) == 1
 
-        # Secondary water resistance
-        SWR = np.random.binomial(1, 0.6) == 1
-
-        # Roof deck attachment //need to add year condition later
-        if V_ult > 130.0:
-            RDA = '8s' # 8d @ 6"/6"
-        else:
-            RDA = '8d' # 8d @ 6"/12"
-
-        # Roof-wall connection // need to add year condition later
-        if HPR:
-            RWC = 'strap' # Strap
-        else:
-            RWC = 'tnail' # Toe-nail
-
-        # Shutters // need to add year condition later
-        shutters = WBD
-
-        # Garage // need to add AG building descr later
-        if shutters:
-            if True:
-                garage = 'sup' # SFBC 1994
+            # Roof deck attachment //need to add year condition later
+            if V_ult > 130.0:
+                RDA = '8s' # 8d @ 6"/6"
             else:
-                garage = 'no'  # None
-        else:
-            if True:
-                if year_built > 1989:
-                    garage = 'std' # Standard
+                RDA = '8d' # 8d @ 6"/12"
+
+            # Roof-wall connection // need to add year condition later
+            if HPR:
+                RWC = 'strap' # Strap
+            else:
+                RWC = 'tnail' # Toe-nail
+
+            # Shutters // need to add year condition later
+            shutters = WBD
+
+            # Garage // need to add AG building descr later
+            if shutters:
+                if (bldg_desc is not None) and ('AG' in bldg_desc):
+                    garage = 'sup' # SFBC 1994
                 else:
-                    garage = 'wkd' # Weak
+                    garage = 'no'  # None
             else:
-                garage = 'no' # None
+                if (bldg_desc is not None) and ('AG' in bldg_desc):
+                    if year_built > 1989:
+                        garage = 'std' # Standard
+                    else:
+                        garage = 'wkd' # Weak
+                else:
+                    garage = 'no' # None
 
-        # Terrain // need to add terrain checks later (perhaps to the BIM)
-                 #// assume suburban for now
-        terrain = 35
+            # Terrain // need to add terrain checks later (perhaps to the BIM)
+                     #// assume suburban for now
+            terrain = int(100 * z0)
 
-        # temp fix
-        stories = min(stories, 2)
+            bldg_config = 'WSF{stories}_{roof_shape}_{SWR}_{RDA}_{RWC}_{garage}_{shutters}_{terrain}'.format(
+                    stories = stories,
+                    roof_shape = roof_type,
+                    SWR = int(SWR),
+                    RDA = RDA,
+                    RWC = RWC,
+                    garage = garage,
+                    shutters=int(shutters),
+                    terrain=terrain
+                    )
 
-        bldg_config = 'WSF{stories}_{roof_shape}_{SWR}_{RDA}_{RWC}_{garage}_{shutters}_{terrain}'.format(
-                stories = stories,
-                roof_shape = roof_type,
-                SWR = int(SWR),
-                RDA = RDA,
-                RWC = RWC,
-                garage = garage,
+        # attributes for WMUH 1-3
+        else:
+            # Secondary water resistance
+            if roof_type == 'flt':
+                SWR = 'null'
+            else:
+                SWR = int(np.random.binomial(1, 0.6) == 1)
+
+            # Roof cover
+            if roof_type in ['gab', 'hip']:
+                roof_cover = 'null'
+            else:
+                if year_built < 1975:
+                    roof_cover = 'bur'
+                else:
+                    roof_cover = 'spm'
+
+            # Roof quality
+            if roof_type in ['gab', 'hip']:
+                roof_quality = 'null'
+            else:
+                if roof_cover == 'bur':
+                    if year_built < 1989:
+                        roof_quality = 'por'
+                    else:
+                        roof_quality = 'god'
+                elif roof_cover == 'spm':
+                    if year_built < 1984:
+                        roof_quality = 'por'
+                    else:
+                        roof_quality = 'god'
+
+
+            # Roof deck attachment //need to add year condition later
+            if z0 == 0.35:
+                if V_asd > 130.0:
+                    RDA = '8s'  # 8d @ 6"/6"
+                else:
+                    RDA = '8d'  # 8d @ 6"/12"
+            else:  # this would be light suburban, but we lump everything here for now
+                if V_asd > 110.0:
+                    RDA = '8s'  # 8d @ 6"/6"
+                else:
+                    RDA = '8d'  # 8d @ 6"/12"
+
+            # Roof-wall connection // need to add year condition later
+            if V_asd > 110.0:
+                RWC = 'strap'  # Strap
+            else:
+                RWC = 'tnail'  # Toe-nail
+
+            # Shutters // need to add year condition later
+            shutters = WBD
+
+            # Terrain // need to add terrain checks later (perhaps to the BIM)
+            # // assume suburban for now
+            terrain = int(100 * z0)
+
+            stories = min(stories, 3)
+
+            bldg_config = 'WMUH{stories}_{roof_shape}_{roof_cover}_{roof_quality}_{SWR}_{RDA}_{RWC}_{shutters}_{terrain}'.format(
+                stories=stories,
+                roof_shape=roof_type,
+                SWR=SWR,
+                roof_cover = roof_cover,
+                roof_quality = roof_quality,
+                RDA=RDA,
+                RWC=RWC,
                 shutters=int(shutters),
                 terrain=terrain
-                )
+            )
 
         loss_dict = {
             '_method': DL_method,
-            'BuildingDamage': {
-                'ReplacementCost': 100,
+            'DamageModel': {
+                'StructureType': struct_type
             },
-            "BuildingResponse": {
-                "EDP_Distribution": "empirical"
+            'LossModel': {
+                'DecisionVariables': {
+                    'ReconstructionCost': True
+                },
+                'ReplacementCost': 100
             },
-            "Components": [
-                {
-                    "ID": bldg_config #"WSF2_gab_1_6s_tnail_no_1_35"
-                }
-            ],
-            "DecisionVariables": {
-                "ReconstructionCost": True
-            },
-            'UncertaintyQuantification': {
+            'ResponseModel': {
+                'ResponseDescription': {
+                    'EDP_Distribution': 'empirical',
                 'Realizations': realization_count
             }
+            },
+            'Components':{
+                bldg_config: []
+            }
         }
+
+    elif DL_method == 'FEMA P58':
+        if BIM_in.get('asset_type',None) == 'Water_Pipe':
+
+            material = BIM_in['material']
+
+            if material in ['Asbestos cement', 'Cast iron']:
+                # brittle material
+                config = 'P0001a'
+            else:
+                # ductile material
+                config = 'P0001b'
+
+            segment_count = BIM_in['segment_count']
+            segment_length = BIM_in['segments'][0]['length']
+            cg_count = int(segment_length / (100 * ft))
+            quantities = '1'
+            for s in range(1, cg_count):
+                quantities += ', 1'
+
+            loss_dict = {
+                "_method"            : "FEMA P58",
+                "ResponseModel"      : {
+                    "ResponseDescription": {
+                        "EDP_Distribution" : "empirical",
+                        "Realizations"     : "1000",   # need to fix this later
+                        "CoupledAssessment": True
+                    }
+                },
+                "DamageModel"        : {
+                    "CollapseProbability": {
+                        "Value": "0.0",
+                    },
+                },
+                "LossModel"          : {
+                    "ReplacementCost"  : "1",
+                    "ReplacementTime"  : "180",
+                    "DecisionVariables": {
+                        "Injuries"          : False,
+                        "ReconstructionCost": True,
+                        "ReconstructionTime": True,
+                        "RedTag"            : False
+                    },
+                },
+                "Dependencies"       : {
+                    "CostAndTime"        : True,
+                    "Fragilities"        : "btw. Damage States",
+                    "Quantities"         : "Independent",
+                    "ReconstructionCosts": "Independent",
+                    "ReconstructionTimes": "Independent",
+                },
+                "ComponentDataFolder": "c:/Adam/Dropbox/Kutatas/2019 SC Testbeds/Memphis/",
+                "Components"         : {
+                    config: [
+                        {
+                            "location"       : "all",
+                            "direction"      : "1",
+                            "median_quantity": quantities,
+                            "unit"           : "ea",
+                            "distribution"   : "N/A",
+                        }
+                    ],
+                }
+            }
 
     DL_input.update({'DamageAndLoss':loss_dict})
 
