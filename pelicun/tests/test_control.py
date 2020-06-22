58f323bb164e9fa409b68996e1bc2d952f4477e8
Adam Zs <33822153+zsarnoczay@users.noreply.github.com>
2020-06-21 18:58:44 -0700

    azs - test travis

--- pelicun/tests/test_control.py
+++ pelicun/tests/test_control.py
@@ -79,7 +79,9 @@ def test_FEMA_P58_Assessment_FRAG_uncertainty_dependencies():
     DL_input = base_input_path + 'input data/' + "DL_input_test_9.json"
     EDP_input = base_input_path + 'EDP data/' + "EDP_table_test_9.out"
 
-    for dep in ['IND', 'PG', 'DIR']: #, 'LOC', 'ATC', 'CSG', 'DS']:
+    for dep in ['IND', 'PG', 'DIR', 'LOC']: #, 'ATC', 'CSG', 'DS']:
+
+        print(f"still running: {dep}")
 
         A = FEMA_P58_Assessment()
 
