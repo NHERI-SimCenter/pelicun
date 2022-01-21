906ee625c8d1763f12d1922b6fa06148bee53876
Adam <33822153+zsarnoczay@users.noreply.github.com>
2022-01-21 11:29:38 -0800

    azs - update versions of dependencies and drop upper bound on xlrd

--- setup.py
+++ setup.py
@@ -34,8 +34,7 @@ setup(
         'numpy>=1.21.0',
         'scipy>=1.7.0',
         'pandas>=1.3.0',
-        'tables',
-        'xlrd<=1.2.0'
+        'tables'
     ],
     classifiers = [
         'Programming Language :: Python',
