cdd9835f1828517d5162637c7b5851f8ed1c6156
Adam Zsarnoczay <33822153+zsarnoczay@users.noreply.github.com>
2023-08-05 17:25:37 -0700

    azs - DB - relax the assumption on mandatory component descriptions If no component description is provided, we just move on with an empty string.

--- pelicun/db.py
+++ pelicun/db.py
@@ -298,8 +298,11 @@ def create_FEMA_P58_fragility_db(source_file,
 
         # store the global (i.e., not DS-specific) metadata
 
-        # every component is assumed to have a comp. description
-        comments = cmp_meta['Component_Description']
+        # start with a comp. description
+        if not pd.isna(cmp_meta['Component_Description']):
+            comments = cmp_meta['Component_Description']
+        else:
+            comments = ''
 
         # the additional fields are added to the description if they exist
 
@@ -711,8 +714,11 @@ def create_FEMA_P58_bldg_repair_db(
 
         # store the global (i.e., not DS-specific) metadata
 
-        # every component is assumed to have a comp. description
-        comments = cmp_meta['Component_Description']
+        # start with a comp. description
+        if not pd.isna(cmp_meta['Component_Description']):
+            comments = cmp_meta['Component_Description']
+        else:
+            comments = ''
 
         # the additional fields are added to the description if they exist
         if cmp_meta['Construction_Quality'] != 'Not Specified':
@@ -1219,8 +1225,11 @@ def create_FEMA_P58_bldg_injury_db(
 
         # store the global (i.e., not DS-specific) metadata
 
-        # every component is assumed to have a comp. description
-        comments = cmp_meta['Component_Description']
+        # start with a comp. description
+        if not pd.isna(cmp_meta['Component_Description']):
+            comments = cmp_meta['Component_Description']
+        else:
+            comments = ''
 
         # the additional fields are added to the description if they exist
         if cmp_meta['Construction_Quality'] != 'Not Specified':
@@ -1595,8 +1604,11 @@ def create_FEMA_P58_bldg_redtag_db(
 
         # store the global (i.e., not DS-specific) metadata
 
-        # every component is assumed to have a comp. description
-        comments = cmp_meta['Component_Description']
+        # start with a comp. description
+        if not pd.isna(cmp_meta['Component_Description']):
+            comments = cmp_meta['Component_Description']
+        else:
+            comments = ''
 
         # the additional fields are added to the description if they exist
         if cmp_meta['Construction_Quality'] != 'Not Specified':
