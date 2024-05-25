468b02d3d156a08aa27bd2d97ecf05aea7e10200
John Vouvakis Manousakis <ioannis_vm@berkeley.edu>
2024-05-25 10:27:18 -0700

    Explain current validation tests.

--- pelicun/tests/validation/0/test_loss_function.py
+++ pelicun/tests/validation/0/test_loss_function.py
@@ -41,9 +41,10 @@
 """
 Validation test on loss functions.
 
-In this example, the loss function is a 1:1 mapping of the input EDP.
-This means that the resulting loss distribution will be the same as
-the EDP input distribution.
+In this example, a single loss function is defined as a 1:1 mapping of
+the input EDP.  This means that the resulting loss distribution will
+be the same as the EDP distribution, allowing us to test and confirm
+that this is what happens.
 
 """
 
