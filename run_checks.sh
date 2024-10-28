d36706ab5d2ea5d9f00b3b1099d7216e7c35758c
John Vouvakis Manousakis <ioannis_vm@berkeley.edu>
2024-10-28 11:53:07 -0700

    Adjust configuration and code checking workflow.
    - Completely ignore certain ruleset files (instead of only ignoring
      checking certain Ruff rules).
    - Add check for formatting in `run_checks.sh`.

--- run_checks.sh
+++ run_checks.sh
@@ -9,12 +9,21 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+# Check formatting with ruff
+echo "Checking formatting with 'ruff format --diff'."
+echo
+ruff format --diff
+if [ $? -ne 0 ]; then
+    echo "ruff format failed."
+    exit 1
+fi
+
 # Run ruff for linting
 echo "Linting with 'ruff check --fix'."
 echo
 ruff check --fix --output-format concise
 if [ $? -ne 0 ]; then
-    echo "ruff failed."
+    echo "ruff check failed."
     exit 1
 fi
 
