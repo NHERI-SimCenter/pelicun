5abfbc310d4cef047be36a52f2dc959b5059ce14
John Vouvakis Manousakis <ioannis_vm@berkeley.edu>
2024-10-31 11:26:41 -0700

    Add code in a `main` method.
    Add code in a `main` method to avoid executing the code when
    generating the API docs.

--- pelicun/tests/validation/inactive/3d_interpolation.py
+++ pelicun/tests/validation/inactive/3d_interpolation.py
@@ -43,36 +43,42 @@ import numpy as np
 import pandas as pd
 from scipy.interpolate import RegularGridInterpolator
 
-# Define domains
-num_samples = 100
-dom1 = np.linspace(0, 1, num_samples)
-dom2 = np.linspace(0, 1, num_samples)
-dom3 = np.linspace(0, 1, num_samples)
 
-# Define 3D array
-vg1, vg2, vg3 = np.meshgrid(dom1, dom2, dom3)
-values = vg1 + np.sqrt(vg2) + np.sin(vg3)
+def main():
+    # Define domains
+    num_samples = 100
+    dom1 = np.linspace(0, 1, num_samples)
+    dom2 = np.linspace(0, 1, num_samples)
+    dom3 = np.linspace(0, 1, num_samples)
 
-# Define test inputs for interpolation.
-x1 = np.random.rand(10)
-x2 = np.random.rand(10)
-x3 = np.random.rand(10)
-test_values = np.column_stack((x1, x2, x3))
+    # Define 3D array
+    vg1, vg2, vg3 = np.meshgrid(dom1, dom2, dom3)
+    values = vg1 + np.sqrt(vg2) + np.sin(vg3)
 
-# Create the interpolation function
-interp_func = RegularGridInterpolator((dom1, dom2, dom3), values)
+    # Define test inputs for interpolation.
+    x1 = np.random.rand(10)
+    x2 = np.random.rand(10)
+    x3 = np.random.rand(10)
+    test_values = np.column_stack((x1, x2, x3))
 
-# Perform the interpolation
-interpolated_value = interp_func(test_values)
+    # Create the interpolation function
+    interp_func = RegularGridInterpolator((dom1, dom2, dom3), values)
 
-# Compare output with the exact value.
-df = pd.DataFrame(
-    {
-        'exact': x1 + np.sqrt(x2) + np.sin(x3),
-        'interpolated': interpolated_value,
-    }
-)
-print(df)
+    # Perform the interpolation
+    interpolated_value = interp_func(test_values)
 
-# Note: This does work with a 2D case, and it could scale to more than
-# 3 dimensions.
+    # Compare output with the exact value.
+    df = pd.DataFrame(
+        {
+            'exact': x1 + np.sqrt(x2) + np.sin(x3),
+            'interpolated': interpolated_value,
+        }
+    )
+    print(df)
+
+    # Note: This does work with a 2D case, and it could scale to more than
+    # 3 dimensions.
+
+
+if __name__ == '__main__':
+    main()
