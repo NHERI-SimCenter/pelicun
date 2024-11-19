8ec6cfda7dfeb14bb7f6d9725cc65f7433ca103d
John Vouvakis Manousakis <ioannis_vm@berkeley.edu>
2024-11-19 13:20:18 -0800

    Address issues with Py3.12
    This commit addresses errors and warnings observed when running our
    test suite with Python version 3.12. They are summarized as follows:
    
    Addressed warnings
    - `datetime.datetime.utcnow().astimezone().tzinfo` is
    deprecated. Using the modern equivalent
    `datetime.now(timezone.utc).astimezone().tzinfo`.
    - `.loc[idx[cols, :, :], xyz]` was resulting in an error in Python
    3.12. Modified to `.loc[list(cols), xyz]`, which is equivalent. Note
    that this syntax only works when the first item in `.loc` is a list,
    so the tuple `cols` is cast to a list.
    - A couple of performance warnings on the operation
    `dv_units.loc[dv_type, cmp_id] = cmp_units.loc[cmp_id, dv_type]`
    in `loss_model.py:1727`, resolved by sorting the index of both dataframes.

--- pelicun/base.py
+++ pelicun/base.py
@@ -43,12 +43,12 @@
 from __future__ import annotations
 
 import argparse
-import datetime
 import json
 import pprint
 import sys
 import traceback
 import warnings
+from datetime import datetime, timezone
 from pathlib import Path
 from typing import TYPE_CHECKING, Any, ClassVar, Optional, TypeVar, overload
 
@@ -412,7 +412,9 @@ class Logger:
 
         for msg_i, msg_line in enumerate(msg_lines):
             if prepend_timestamp and (msg_i == 0):
-                formatted_msg = f'{datetime.datetime.now().strftime(self.log_time_format)} {msg_line}'  # noqa: DTZ005
+                formatted_msg = (
+                    f'{datetime.now().strftime(self.log_time_format)} {msg_line}'  # noqa: DTZ005
+                )
             elif prepend_timestamp or prepend_blank_space:
                 formatted_msg = self.spaces + msg_line
             else:
@@ -487,9 +489,9 @@ class Logger:
         self.msg(
             'System Information:', prepend_timestamp=False, prepend_blank_space=False
         )
-        start = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')  # noqa: DTZ005
+        start = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')  # noqa: DTZ005
         self.msg(
-            f'local time zone: {datetime.datetime.utcnow().astimezone().tzinfo}\n'
+            f'local time zone: {datetime.now(timezone.utc).astimezone().tzinfo}\n'
             f'start time: {start}\n'
             f'python: {sys.version}\n'
             f'numpy: {np.__version__}\n'
